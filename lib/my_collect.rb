922c5f749edae4c95461f6402e91b88fb8d83cce
Monica Gerard <mlgerardvla@gmail.com>
2020-03-13 20:34:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+  index  = 0
+  collection = []
 
+  while (index < array.length)
+    collection << yield(array[index])
+    index += 1
+  end
+  collection
+end
 
+array = []
+
+my_collect(array) do |name|
+  name.upcase
+end
