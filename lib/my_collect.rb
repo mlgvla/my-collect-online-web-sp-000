9e09fa43eb2a5bfb60085f56b6903ef3b7eb84e4
Monica Gerard <mlgerardvla@gmail.com>
2020-03-13 20:32:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,15 @@
 def my_collect(array)
+  index  = 0
+  collection = []
 
+  while (index < array.length)
+    collection << yield(array[i])
+  end
+  collection
 end
 
 array = []
 
 my_collect(array) do |name|
-
+  name.upcase
 end
